8c653d5e50edd7d2501c0b4aa3f7af709fcbe764
M-Robledo66 <miguelrobledo773@gmail.com>
2022-12-09 15:22:53 -0600

    ttt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 /*-------------------------------- Constants --------------------------------*/
+let board = [null, null, null, null, null, null, null, null, null]
+
+//  '1' represents player X
+let turn = '1'
+
+let winner = false
+
+let tie = false
+ 
 
 
 
@@ -8,11 +17,34 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.getElementsByClassName('board')
 
-
+const messageEl = document.getElementById('message')
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+// init function will initialize the game state of player
+function init(params) {
+ 
+   function render()
+}
+function render (params) {
+}
+function updateBoard(params) {
+    for (i = 0; i < board.length; i++){
+        let currentSqr = square[i]
+        currentSqr.textContent = ""
+        if (board[i]=== -1){
+            currentSqr.textContent = "O"
+        } else if(board[i] ===1){
+            currentSqr.textContent = "X"
+        }
+    }
+}
+
+ function updateMessage()
 
